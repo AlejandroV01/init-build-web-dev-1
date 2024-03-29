74729205edfdc6e72d4212d2fd74e561c802cf20
Alejandro Vera <alexvera0109@gmail.com>
2024-03-29 15:38:29 -0400

    Update Home.tsx

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,10 +1,8 @@
-import HomeSearchInput from '@/components/HomeSearchInput'
 import React from 'react'
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <HomeSearchInput />
     </div>
   )
 }
