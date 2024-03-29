b93d96c2f34025c172a5bf53c28cc1a9a41bb299
Alejandro Vera <alexvera0109@gmail.com>
2024-03-29 17:44:39 -0400

    base code for cta

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,8 +1,10 @@
+import CallToAction from '@/components/CallToAction'
 import React from 'react'
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <CallToAction />
     </div>
   )
 }
