4d9f77ebb7152225921959fa428d62c2a10f8e06
Alejandro Vera <alexvera0109@gmail.com>
2024-03-30 00:50:21 -0400

    redux base code

--- client/src/components/SignUpSteps/EducationStep.tsx
+++ client/src/components/SignUpSteps/EducationStep.tsx
@@ -1,6 +1,8 @@
+import { useAppSelector } from '@/store/hooks'
 import React from 'react'
-
 const EducationStep = () => {
+  const user = useAppSelector(state => state.auth)
+  console.log('This is the user_id:', user.id)
   return (
     <div>
       <h1>EducationStep</h1>
