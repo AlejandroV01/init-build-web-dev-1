dd4a9799ebdc19952ff353c029d9ac2a25448ea1
Alejandro Vera <alexvera0109@gmail.com>
2024-03-30 00:31:53 -0400

    needed queries

---
+++ client/src/database/educations/insertEducation.ts
@@ -0,0 +1,27 @@
+import supabase from "@/lib/supabaseClient";
+import { IEducationTableTypes } from "./../../types/index";
+
+const insertEducation = async (education: IEducationTableTypes) => {
+  const { data, error } = await supabase
+    .from("education")
+    .insert([
+      {
+        education_id: education.education_id,
+        profile_id: education.profile_id,
+        school: education.school,
+        major: education.major,
+        start_date: education.start_date,
+        end_date: education.end_date,
+      },
+    ])
+    .select();
+  if (data) {
+    console.log(data);
+    return true;
+  } else {
+    console.error(error);
+    return false;
+  }
+};
+
+export default insertEducation;
---
+++ client/src/database/experiences/insertExperience.ts
@@ -0,0 +1,28 @@
+import supabase from "@/lib/supabaseClient";
+import { IExperienceTableTypes } from "./../../types/index";
+
+const insertExperience = async (experience: IExperienceTableTypes) => {
+  const { data, error } = await supabase
+    .from("experience")
+    .insert([
+      {
+        profile_id: experience.profile_id,
+        experience_id: experience.experience_id,
+        company: experience.company,
+        title: experience.title,
+        start_date: experience.start_date,
+        end_date: experience.end_date,
+        description: experience.description,
+      },
+    ])
+    .select();
+  if (data) {
+    console.log(data);
+    return true;
+  } else {
+    console.error(error);
+    return false;
+  }
+};
+
+export default insertExperience;
