2f7bcf39f2db0fba8bcc85ec0215878d82233846
Alejandro Vera <alexvera0109@gmail.com>
2024-03-30 21:39:00 -0400

    auth changes to login and signup logic

--- client/src/routes/Auth.tsx
+++ client/src/routes/Auth.tsx
@@ -1,39 +1,10 @@
-import Button from '@/components/Button'
-import LoginCard from '@/components/LoginCard'
-import supabase from '@/lib/supabaseClient'
-import { useAppSelector } from '@/store/hooks'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { Session } from '@supabase/supabase-js'
-import React, { useEffect, useState } from 'react'
+import { LoginCard, SignUpCard } from '@/components/AuthComponents'
 import { useSearchParams } from 'react-router-dom'
 const Auth = () => {
   const [params] = useSearchParams()
   const isLogin = params.get('login')
-  const [session, setSession] = useState<null | Session>(null)
-  // useEffect(() => {
-  //   const {
-  //     data: { subscription },
-  //   } = supabase.auth.onAuthStateChange((_event, session) => {
-  //     setSession(session)
-  //   })
-  //   console.log('running auth effect')
-  //   return () => subscription.unsubscribe()
-  // }, [])
-  console.log(session?.user)
-  // const handleSignOut = async () => {
-  //   const { error } = await supabase.auth.signOut()
-  //   if (error) {
-  //     console.error('Error signing out:', error.message)
-  //   }
-  // }
-  return (
-    <div className='flex items-center justify-center h-screen w-screen bg-green-100'>
-      {session ? <p>Signed In</p> : <LoginCard isLogin={isLogin} />}
-      {/* <Button variant='primary' onClick={handleSignOut}>
-        Sign Out
-      </Button> */}
-    </div>
-  )
+
+  return <div className='flex flex-col items-center container '>{isLogin === 'true' ? <LoginCard /> : <SignUpCard />}</div>
 }
 
 export default Auth
--- client/src/routes/Root.tsx
+++ client/src/routes/Root.tsx
@@ -1,11 +1,19 @@
 import Nav from '@/components/Nav'
+import { useRetrieveUser } from '@/hooks/useRetrieveUser'
+import { addProfile } from '@/store/auth/auth.slice'
+import { useAppDispatch } from '@/store/hooks'
 import React from 'react'
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+
 const Root = () => {
-  const location = useLocation()
+  const dispatch = useAppDispatch()
+  const { user, loading } = useRetrieveUser()
+  console.log(user, loading)
+  if (loading) return <div>Loading...</div>
+
   return (
     <main className='flex flex-col'>
-      {location.pathname !== '/auth' && <Nav />}
+      <Nav />
       <Outlet />
     </main>
   )
