15048bb0ec70e0b62837ff381d6a79f1190d1b71
Alejandro Vera <alexvera0109@gmail.com>
2024-04-03 14:45:26 -0400

    updated updateApplicants

--- client/src/database/idea_applicants/updateApplicantByIdeaId.ts
+++ client/src/database/idea_applicants/updateApplicantByIdeaId.ts
@@ -1,57 +1,27 @@
-import { Enums } from "@/lib/supabase";
-import supabase from "@/lib/supabaseClient";
-import { error} from "console";
-import { useState } from "react";
+import supabase from '@/lib/supabaseClient'
 
-enum RoleType {
-  frontend = 0,
-  backend = 1,
-  fullstack = 2,
-  ui_ux = 3
+interface IIdeaApplicants {
+  idea_id: number
+  profile_id: number
+  application_role: 'Frontend' | 'Backend' | 'UI/UX' | 'Full-Stack'
 }
 
-/* const switchApplicationRole = () => {
-  const [currentRole, setRole] = useState(RoleType.fullstack)
-  switch (currentRole) {
-    case RoleType.frontend:
-      return 'Frontend'
-    case RoleType.backend:
-      return 'Backend'
-    case RoleType.fullstack:
-      return 'Full-Stack'
-    case RoleType.ui_ux:
-      return 'UI/UX'      
+const updateIdeaApplicants = async (newApplicantUpdate: IIdeaApplicants) => {
+  const { data: ideas, error } = await supabase
+    .from('idea_applicants')
+    .update({
+      application_role: newApplicantUpdate.application_role,
+    })
+    .eq('idea_id', newApplicantUpdate.idea_id)
+    .eq('profile_id', newApplicantUpdate.profile_id)
+    .select('*')
+
+  if (ideas) {
+    return ideas
+  } else {
+    console.error(error)
+    return null
   }
-} */
+}
 
-
-//Potential interface interface
-/* interface IIdeaApplicants {
-  idea_id: number,
-  profile_id: number,
-  application_role: RoleType
-} */
-
-
-
-const updateIdeaApplicants = async (idea_id: number, newProfileId: number, newApplicationRole: number) => {
-    const { data: ideas, error } = await supabase
-      .from('idea_applicants')
-      .select("*")
-      .update(
-        {
-          profile_id: newProfileId,
-          application_role: newApplicationRole
-        }
-      )
-      .eq("idea_id", idea_id);
-  
-    if (ideas) {
-      return ideas;
-    } else {
-      console.error(error);
-      return null;
-    }
-  };
-
-export default updateIdeaApplicants;
+export default updateIdeaApplicants
