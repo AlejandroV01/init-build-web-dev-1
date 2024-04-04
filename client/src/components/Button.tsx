90dabc47b9cab84607418a7fd4984e881c5990d9
Alejandro Vera <alexvera0109@gmail.com>
2024-04-03 23:11:42 -0400

    Update Button.tsx

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -2,10 +2,9 @@ interface ButtonProps {
   variant: 'primary' | 'secondary' | 'success' | 'destructive'
   disabled?: boolean
   onClick?: () => void
-  children: string
 }
 
-const Button = ({ variant, disabled = false, onClick, children }: ButtonProps) => {
+const Button = ({ variant, disabled = false, onClick }: ButtonProps) => {
   const checkButtonVariance = () => {
     switch (variant) {
       case 'primary':
@@ -19,9 +18,8 @@ const Button = ({ variant, disabled = false, onClick, children }: ButtonProps) =
 
   return (
     <button disabled={disabled} className={`${checkButtonVariance()} px-3 py-1.5 text-white rounded font-medium w-fit`} onClick={onClick}>
-      {children}
+      text
     </button>
   )
 }
-
 export default Button
