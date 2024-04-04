0f33c22811fd151510d6fba6afebde71d5f52081
Alejandro Vera <alexvera0109@gmail.com>
2024-04-03 23:11:14 -0400

    Update Home.tsx

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,19 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 //changed Badge file name
-import SkillCard from '@/components/SkillCard'
 const Home = () => {
-  const [profileSkills, setProfileSkills] = useState(['React', 'TypeScript'])
-
-  const changeProfileSkills = (event: React.FormEvent<HTMLFormElement>, newSkill: string) => {
-    event.preventDefault()
-    setProfileSkills([...profileSkills, newSkill])
-  }
   return (
     <div>
       <h1>Home</h1>
-      <SkillCard skills={profileSkills} onDoneClick={changeProfileSkills} />
     </div>
   )
 }
-
 export default Home
