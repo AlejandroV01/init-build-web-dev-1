71a57b709d1e2e76325f79c07faccd8984580917
salimaduen <salimaduen@hotmail.com>
2024-04-07 23:39:58 -0400

    Added skeleton and skeleton for the profile loading page

--- client/src/components/ui/ProfileLoadingSkeleton.tsx
+++ client/src/components/ui/ProfileLoadingSkeleton.tsx
@@ -1,28 +1,36 @@
 import Skeleton from "./skeleton";
 
 const ProfileSkeleton = () => {
-    return (
-        <div className='w-full flex flex-col'>
-            <div className='flex gap-4 mb-4'>
-                <Skeleton skeletonStyling='h-16 w-16 rounded-full'/>
-                <div className="flex flex-col justify-between my-2">
-                    <div className="flex flex-col gap-1">
-                    <Skeleton skeletonStyling="w-[100px] h-[8px] rounded-lg"/>
-                    
-                    <Skeleton skeletonStyling="w-[100px] h-[8px] rounded-lg"/>
-                    </div>
-                    <Skeleton skeletonStyling="w-[100px] h-[8px] rounded-lg"/>
+  return (
+    <div className="w-full flex flex-col">
+      <div className="flex gap-4 mb-4">
+        <Skeleton skeletonStyling="h-16 w-16 rounded-full" />
+        <div className="flex flex-col justify-between my-2">
+          <div className="flex flex-col gap-1">
+            <Skeleton skeletonStyling="w-[100px] h-[8px] rounded-lg" />
 
-                </div>
-            </div>
-            <div className='flex flex-col h-full gap-2'>
-                <Skeleton skeletonStyling='h-[8px] w-full rounded-lg'/>
-                <Skeleton skeletonStyling='h-[8px] w-full rounded-lg'/>
-                <Skeleton skeletonStyling='h-[8px] w-full rounded-lg'/>
-                <Skeleton skeletonStyling='h-[8px] w-full rounded-lg'/>
-            </div>
+            <Skeleton skeletonStyling="w-[100px] h-[8px] rounded-lg" />
+          </div>
+          <Skeleton skeletonStyling="w-[100px] h-[8px] rounded-lg" />
         </div>
-    );
+      </div>
+      <div className="flex flex-col h-full gap-2">
+        <Skeleton skeletonStyling="h-[8px] w-full rounded-lg" />
+        <Skeleton skeletonStyling="h-[8px] w-full rounded-lg" />
+        <Skeleton skeletonStyling="h-[8px] w-full rounded-lg" />
+        <Skeleton skeletonStyling="h-[8px] w-full rounded-lg" />
+      </div>
+    </div>
+  );
+};
+
+const ProfileLoadingSkeleton = () => {
+  return (
+    <div className="flex items-center space-x-4">
+      <Skeleton skeletonStyling="h-12 w-12 rounded-full" />
+      <div className="space-y-3">{ExperienceSkeleton()}</div>
+    </div>
+  );
 };
 
 export default ProfileSkeleton;
