d9cd53e5f65a89f4eb44681be360b47f03907b89
Alejandro Vera <alexvera0109@gmail.com>
2024-04-08 19:32:26 -0400

    Update Home.tsx

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,62 +1,9 @@
-import ApplicantsPopup from "@/components/ApplictantsPopup";
 import React from "react";
-
-export interface IApplicants {
-  id: number;
-  firstName: string;
-  lastName: string;
-  role: string;
-}
 const Home = () => {
-  const mockApplicants = [
-    {
-      id: 1,
-      firstName: "John",
-      lastName: "Doe",
-      role: "Software Engineer",
-    },
-    {
-      id: 2,
-      firstName: "Jane",
-      lastName: "Doe",
-      role: "Product Manager",
-    },
-    {
-      id: 3,
-      firstName: "Mike",
-      lastName: "Smith",
-      role: "Software Engineer",
-    },
-    {
-      id: 4,
-      firstName: "Daniela",
-      lastName: "Smith",
-      role: "Product Manager",
-    },
-  ];
   return (
     <div>
       <h1>Home</h1>
-      <ApplicantsPopup applicants={mockApplicants} />
     </div>
   );
 };
 export default Home;
-
-const CTA = () => {
-  return (
-    <div className="bg-primary py-10">
-      <div className="flex flex-col items-center gap-5 container">
-        <h2 className="text-3xl font-semibold text-center text-white">
-          Get your ideas out there.
-        </h2>
-        <a href="/auth">
-          <Button variant="secondary" className="text-white bg-[#928bff]">
-            Get Started
-          </Button>
-        </a>
-        <img src="/MAC.webp" alt="" className="w-full max-w-[900px]" />
-      </div>
-    </div>
-  );
-};
