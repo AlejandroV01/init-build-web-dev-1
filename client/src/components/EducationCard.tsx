19f0dca9692a0395974a797fd01e2fe1093f3fca
Alejandro Vera <alexvera0109@gmail.com>
2024-04-08 19:31:38 -0400

    Update EducationCard.tsx

--- client/src/components/EducationCard.tsx
+++ client/src/components/EducationCard.tsx
@@ -1,9 +1,15 @@
-import Education from "@/components/Education";
 import React from "react";
+import Education from "@/components/Education";
 
+import { HiPencil } from "react-icons/hi2";
 import { HiPencil } from "react-icons/hi2";
 
 interface Educations {
+  schoolLogo: string;
+  schoolName: string;
+  schoolMajor: string;
+  startDate: string;
+  endDate: string;
   schoolLogo: string;
   schoolName: string;
   schoolMajor: string;
@@ -21,8 +27,12 @@ const ExperienceCard: React.FC<EducationCardProps> = ({ educations }) => {
       <div className="flex justify-between items-center">
         <span className="font-extrabold text-2xl text-black">Education</span>
         <HiPencil color="#7B7B7B" fontSize="18px" />
+    <div className="w-[850px] py-5 px-5 pt-4 flex flex-col gap-7 rounded-[8px] drop-shad bg-[#E8E8E8]">
+      <div className="flex justify-between items-center">
+        <span className="font-extrabold text-2xl text-black">Education</span>
+        <HiPencil color="#7B7B7B" fontSize="18px" />
       </div>
-      <div className="flex flex-col gap-7" onSubmit={(e) => console.log(e)}>
+      <div className="flex flex-col gap-7">
         {educations.map((edu, index) => (
           <Education
             key={index}
