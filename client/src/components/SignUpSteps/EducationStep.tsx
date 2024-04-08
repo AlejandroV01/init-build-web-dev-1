ce70f463e4b8854e61ee2d56dabff166452e0af0
Alejandro Vera <alexvera0109@gmail.com>
2024-04-08 14:48:33 -0400

    Update EducationStep.tsx

--- client/src/components/SignUpSteps/EducationStep.tsx
+++ client/src/components/SignUpSteps/EducationStep.tsx
@@ -1,13 +1,10 @@
-import { useAppSelector } from '@/store/hooks'
 import React from 'react'
+
 const EducationStep = () => {
-  const user = useAppSelector(state => state.auth)
-  console.log('This is the user_id:', user.id)
   return (
     <div>
       <h1>EducationStep</h1>
     </div>
   )
 }
-
 export default EducationStep
