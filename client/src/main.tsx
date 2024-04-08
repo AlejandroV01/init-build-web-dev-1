7446c73c4f5e747bb57c25c850cea46d28bc73c0
dab1an <dabianextras@gmail.com>
2024-04-08 01:28:42 -0400

    chat route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,8 +12,10 @@ import Home from "./routes/Home.tsx";
 import Idea from "./routes/Idea.tsx";
 import Ideas from "./routes/Ideas.tsx";
 import Root from "./routes/Root.tsx";
-import UserSetup from "./routes/UserSetup.tsx";
+import DevChats from "./components/DevChats.tsx";
+
 import StoreProvider from "./store/StoreProvider.tsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -41,8 +43,8 @@ const router = createBrowserRouter([
         element: <Idea />,
       },
       {
-        path: "/user-setup",
-        element: <UserSetup />,
+        path: "devchats",
+        element: <DevChats />,
       },
     ],
   },
