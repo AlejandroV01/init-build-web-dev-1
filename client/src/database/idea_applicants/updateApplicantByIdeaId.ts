82264e57cfa1412e30b72fbd16bb658af3eb0e06
Alejandro Vera <alexvera0109@gmail.com>
2024-04-08 15:22:17 -0400

    fixed idea_applicant db queries for is_accepted

--- client/src/database/idea_applicants/updateApplicantByIdeaId.ts
+++ client/src/database/idea_applicants/updateApplicantByIdeaId.ts
@@ -4,13 +4,14 @@ interface IIdeaApplicants {
   idea_id: number
   profile_id: number
   application_role: 'Frontend' | 'Backend' | 'UI/UX' | 'Full-Stack'
+  is_accepted: boolean
 }
 
 const updateIdeaApplicants = async (newApplicantUpdate: IIdeaApplicants) => {
   const { data: ideas, error } = await supabase
     .from('idea_applicants')
     .update({
-      application_role: newApplicantUpdate.application_role,
+      is_accepted: newApplicantUpdate.is_accepted,
     })
     .eq('idea_id', newApplicantUpdate.idea_id)
     .eq('profile_id', newApplicantUpdate.profile_id)
