d46bd497a82d77dd52945a336b122abe5301afbc
dab1an <dabianextras@gmail.com>
2024-04-08 01:28:42 -0400

    chat route

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -6,17 +6,16 @@ import {
 import supabase from "@/lib/supabaseClient";
 import { removeProfile } from "@/store/auth/auth.slice";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Avatar from "./Avatar";
 import Button from "./Button";
 import HamburgerMenu from "./HamburgerMenu";
 import { ModeToggle } from "./mode-toggle";
+
 const Nav = () => {
   const user = useAppSelector((state) => state.auth);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const location = useLocation();
-  const { pathname } = location;
   const handleSignOut = async () => {
     dispatch(removeProfile());
     const { error } = await supabase.auth.signOut();
@@ -46,53 +45,50 @@ const Nav = () => {
       <a href="/">
         <h2 className="font-bold text-xl">Devbuds</h2>
       </a>
-      {pathname !== "/user-setup" && (
-        <ul className="gap-5 change [&>*]:hover:cursor-pointer font-medium hidden sm:flex">
-          {items.map((item) => (
-            <li key={item.route}>
-              <a href={item.route}>{item.name}</a>
-            </li>
-          ))}
-        </ul>
-      )}
-      {pathname !== "/user-setup" && (
-        <div className="flex gap-5 items-center">
-          {user.profile_id ? (
-            <div className="flex items-center gap-2">
-              <Popover>
-                <PopoverTrigger>
-                  <Avatar
-                    firstName={user.first_name}
-                    lastName={user.last_name}
-                    size={35}
-                  />
-                </PopoverTrigger>
-                <PopoverContent className="w-fit flex flex-col items-start gap-2 pr-10">
-                  <a
-                    className="font-semibold hover:underline cursor-pointer text-foreground/90"
-                    href="/dashboard"
-                  >
-                    My Profile
-                  </a>
-                  <div
-                    className="font-semibold hover:underline cursor-pointer text-foreground/90"
-                    onClick={handleSignOut}
-                  >
-                    Sign Out
-                  </div>
-                </PopoverContent>
-              </Popover>
-              <ModeToggle />
-              <HamburgerMenu />
-            </div>
-          ) : (
-            <>
-              <NavAuthButtons className="hidden sm:flex" />
-              <HamburgerMenu />
-            </>
-          )}
-        </div>
-      )}
+      <ul className="gap-5 change [&>*]:hover:cursor-pointer font-medium hidden sm:flex">
+        {items.map((item) => (
+          <li key={item.route}>
+            <a href={item.route}>{item.name}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="flex gap-5 items-center">
+        {user.profile_id ? (
+          <div className="flex items-center gap-2">
+            <Popover>
+              <PopoverTrigger>
+                {/* href={`/profile/${user.id}`} */}
+                <Avatar
+                  firstName={user.first_name}
+                  lastName={user.last_name}
+                  size={35}
+                />
+              </PopoverTrigger>
+              <PopoverContent className="w-fit flex flex-col items-start gap-2 pr-10">
+                <a
+                  className="font-semibold hover:underline cursor-pointer text-foreground/90"
+                  href="/dashboard"
+                >
+                  My Profile
+                </a>
+                <div
+                  className="font-semibold hover:underline cursor-pointer text-foreground/90"
+                  onClick={handleSignOut}
+                >
+                  Sign Out
+                </div>
+              </PopoverContent>
+            </Popover>
+            <ModeToggle />
+            <HamburgerMenu />
+          </div>
+        ) : (
+          <>
+            <NavAuthButtons className="hidden sm:flex" />
+            <HamburgerMenu />
+          </>
+        )}
+      </div>
     </div>
   );
 };
