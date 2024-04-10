2af54d422ad30cf87c3af5ca741776d1160920a6
Alejandro Vera <alexvera0109@gmail.com>
2024-04-09 21:48:17 -0400

    Update Home.tsx

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,12 +1,9 @@
-import LinkStep from '@/components/SignUpSteps/LinkStep'
 import React from 'react'
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <LinkStep />
     </div>
   )
 }
-
 export default Home
